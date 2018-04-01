92b0d6193aee1b3b25301a844ddd685ee94c62ff
Rui Baiao <rui.baiao@tingcore.com>
2018-04-01 23:33:24 +0200

    header now displays the gradient colors

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var css = document.querySelector('h3');
+var cssHeader = document.querySelector('h3');
 var color1 = document.querySelector('#color1');
 var color2 = document.querySelector('#color2');
 
 var body = document.querySelector('#mainContainer');
 
 function setBackgroundGradient(c1, c2, direction) {
-  body.style.background = `linear-gradient(to ${direction}, ${color1.value}, ${color2.value})`;
+    body.style.background = `linear-gradient(to ${direction}, ${color1.value}, ${color2.value})`;
+    cssHeader.textContent = body.style.background + ';';
 }
