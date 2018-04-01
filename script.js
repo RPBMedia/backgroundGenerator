6a68054015e5ad17e2b5697ed47ce35b8d8b832f
Rui Baiao <rui.baiao@tingcore.com>
2018-04-01 23:31:17 +0200

    cleanup

--- script.js
+++ script.js
@@ -4,11 +4,6 @@ var color2 = document.querySelector('#color2');
 
 var body = document.querySelector('#mainContainer');
 
-console.log(body);
-
 function setBackgroundGradient(c1, c2, direction) {
   body.style.background = `linear-gradient(to ${direction}, ${color1.value}, ${color2.value})`;
 }
-
-// color1.addEventListener('input', () => setBackgroundGradient(color1.value, color2.value, 'right'));
-// color2.addEventListener('input', () => setBackgroundGradient(color1.value, color2.value, 'right'));
